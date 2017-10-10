ce36ab3d31b39cba6eed9b1bb077888e34214b8d
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2017-10-10 18:18:45 -0400

    Add files via upload

---
+++ fruitful.py
@@ -0,0 +1,12 @@
+def sum_to(n):
+    result=(n*(n+1))/2
+    return result
+
+t=sum_to(5)
+print("sum from 1 to 12 is",t)
+
+t=sum_to(10)
+print("sum from 1 to 10 is",t)
+
+t=sum_to(45)
+print("sum from 1 to 45 is",t)
